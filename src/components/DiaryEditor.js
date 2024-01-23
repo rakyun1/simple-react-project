5d4d4114780309a89290f94a0b11df03d4e66bae
rakyun1 <dbskim234@naver.com>
2024-01-23 15:30:22 +0900

    init emotionDiary

---
+++ src/components/DiaryEditor.js
@@ -0,0 +1,126 @@
+import { useCallback, useContext, useEffect, useRef, useState } from "react";
+import { useNavigate } from "react-router-dom";
+
+import MyButton from "./MyButton";
+import MyHeader from "./MyHeader";
+import EmotionItem from "./EmotionItem";
+import { DiaryDispatchContext } from "./../App.js";
+import { getStringDate } from "../utill/date.js";
+import { emotionList } from "../utill/emotion.js";
+
+const DiaryEditor = ({ isEdit, originData }) => {
+  const [emotion, setEmotion] = useState(3);
+  const [date, setDate] = useState(getStringDate(new Date()));
+  const [content, setContent] = useState("");
+  const contentRef = useRef();
+  const { onCreate, onEdit, onRemove } = useContext(DiaryDispatchContext);
+
+  const handleClickEmote = useCallback((emotion) => {
+    setEmotion(emotion);
+  }, []);
+
+  const navigate = useNavigate();
+
+  const handleSubmit = () => {
+    if (content.length < 1) {
+      contentRef.current.focus();
+      return;
+    }
+    if (
+      window.confirm(
+        isEdit ? "일기를 수정하시겠습니까?" : "새로운 일기를 작성하시겠습니까?"
+      )
+    ) {
+      if (!isEdit) {
+        onCreate(date, content, emotion);
+      } else {
+        onEdit(originData.id, date, content, emotion);
+      }
+    }
+
+    navigate("/", { replace: true });
+  };
+
+  const handleRemove = () => {
+    if (window.confirm("정말 삭제하시겠습니까?")) {
+      onRemove(originData.id);
+      navigate("/", { replace: true });
+    }
+  };
+
+  useEffect(() => {
+    if (isEdit) {
+      setDate(getStringDate(new Date(parseInt(originData.date))));
+      setEmotion(originData.emotion);
+      setContent(originData.content);
+    }
+  }, [isEdit, originData]);
+
+  return (
+    <div className="DiaryEditor">
+      <MyHeader
+        headText={isEdit ? "일기 수정하기" : "새로운 일기 쓰기"}
+        leftChild={
+          <MyButton text={"< 뒤로가기"} onClick={() => navigate(-1)} />
+        }
+        rightChild={
+          isEdit && (
+            <MyButton
+              text={"삭제하기"}
+              type={"negative"}
+              onClick={handleRemove}
+            />
+          )
+        }
+      />
+      <div>
+        <section>
+          <h4>오늘은 언제인가요?</h4>
+          <div className="input_box">
+            <input
+              className="input_date"
+              value={date}
+              onChange={(e) => setDate(e.target.value)}
+              type="date"
+            />
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 감정</h4>
+          <div className="input_box emotion_list_wrapper">
+            {emotionList.map((it) => (
+              <EmotionItem
+                key={it.emotion_id}
+                {...it}
+                onClick={handleClickEmote}
+                isSelected={it.emotion_id === emotion}
+              ></EmotionItem>
+            ))}
+          </div>
+        </section>
+        <section>
+          <h4>오늘의 일기</h4>
+          <div className="input_box_text_wrapper">
+            <textarea
+              placeholder="오늘은 어땠나요?"
+              ref={contentRef}
+              value={content}
+              onChange={(e) => setContent(e.target.value)}
+            ></textarea>
+          </div>
+        </section>
+        <section>
+          <div className="control_box">
+            <MyButton text={"취소하기"} onClick={() => navigate(-1)}></MyButton>
+            <MyButton
+              text={"작성완료"}
+              type={"positive"}
+              onClick={handleSubmit}
+            ></MyButton>
+          </div>
+        </section>
+      </div>
+    </div>
+  );
+};
+export default DiaryEditor;
---
+++ src/components/MyHeader.js
@@ -0,0 +1,26 @@
+const env = process.env;
+env.PUBLIC_URL = env.PUBLIC_URL || "";
+
+const MyHeader = ({ headText, leftChild, rightChild }) => {
+  const heartImg = process.env.PUBLIC_URL + `/assets/heartImg.png`;
+
+  return (
+    <header>
+      <div className="heartImgContainer">
+        <div className="heartImg">
+          <img src={heartImg} />
+        </div>
+        <div className="rak">락윤이와 윤주의 사랑 일기</div>
+        <div className="heartImg">
+          <img src={heartImg} />
+        </div>
+      </div>
+      <div className="headerContent">
+        <div className="head_btn_left">{leftChild}</div>
+        <div className="head_text">{headText}</div>
+        <div className="head_btn_right">{rightChild}</div>
+      </div>
+    </header>
+  );
+};
+export default MyHeader;
